11c06803e5b3d5a27e3b08a6b046567694059d10
Muhammad Syarifudden bin Haji Zulkefli 4416 <4416@laksamanacollege.edu.bn>
2022-04-17 02:32:02 +0800

    edit image on the edit page

--- Pages/Admin/edit.cshtml.cs
+++ Pages/Admin/edit.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -21,7 +22,7 @@ namespace Restaurant.Pages.Admin
         public async Task<IActionResult> OnGetAsync(int id)
         {
             item = await _db.Menuitem.FindAsync(id);
-            if(item== null)
+            if (item == null)
             {
                 return RedirectToPage("/Admin/Create");
             }
@@ -31,20 +32,29 @@ namespace Restaurant.Pages.Admin
 
         public async Task<IActionResult> OnPostSaveAsync()
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid) { return Page(); }
+            item.Active = true;
+            foreach (var file in Request.Form.Files)
             {
-                return Page();
+                MemoryStream ms = new MemoryStream();
+                file.CopyTo(ms);
+                item.ImageData = ms.ToArray();
+
+                ms.Close();
+                ms.Dispose();
             }
-            _db.Attach(item).State = EntityState.Modified;
-            try
-            {
-                await _db.SaveChangesAsync();
+                _db.Attach(item).State = EntityState.Modified;
+                try
+                {
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException e)
+                {
+                    throw new Exception($"Item {item.ID} not found!", e);
+                }
+                return RedirectToPage("/Admin/Create");
             }
-            catch (DbUpdateConcurrencyException e)
-            {
-                throw new Exception($"Item {item.ID} not found!", e);
-            }
-            return RedirectToPage("/Admin/Create");
         }
     }
-}
+
+
